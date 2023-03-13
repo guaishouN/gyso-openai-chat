e48e121b2ff4cd7040a06afe39f90745547bb996
guaishouN <674149099@qq.com>
2023-03-13 21:18:13 +0800

    nice

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from flask import Markup
+import markdown
+
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -19,25 +22,37 @@ messages = [
     {"role": "user", "content": "write a python function to read a file"}
 ]
 
+tmpContent= ""
+
+# md转html的方法
+def md2html(mdcontent):
+    exts = ['markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.tables',
+            'markdown.extensions.toc']
+    html = markdown.markdown(mdcontent, extensions=exts)
+    content = Markup(html)
+    return content
+
 @app.route("/", methods=("GET", "POST"))
 def index():
+    global tmpContent
     if request.method == "POST":
         animal = request.form["iSay"]
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=generate_prompt(animal)
         )
-        print(response)
-        return redirect(url_for("index", result=response.choices[0].message.content))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        #print(response)
+        data = response.choices[0].message.content
+        tmpContent = md2html(data)
+        return redirect(url_for("index"))
+    return render_template("index.html", result=tmpContent)
 
 
 def generate_prompt(animal):
     messages[0]["content"] = animal.capitalize()
     return messages
 
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3389)
+    app.run(host="0.0.0.0", port=8000)
